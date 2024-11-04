bb7339131dcba766e6a7d5f142d728157248e33e
Md. Ahanaf Arif Khan <ahanaf019@gmail.com>
2024-11-05 01:02:27 +0600

    Implement simple Unet

--- dataset.py
+++ dataset.py
@@ -7,14 +7,21 @@ from torch.utils.data import DataLoader, Dataset
 from pathlib import Path
 from typing import Optional, List, Dict
 import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 from glob import glob
 
+matplotlib.use("GTK3Agg")
 
 
 class BinSegDataset(Dataset):
-    def __init__(self, image_paths, mask_paths):
+    def __init__(self, image_paths, mask_paths, image_size, patch_size):
         self.image_paths = image_paths
         self.mask_paths = mask_paths
+        self.image_size = image_size
+        self.patch_size = patch_size
     
     
     def __len__(self):
@@ -23,8 +30,102 @@ class BinSegDataset(Dataset):
     
     
     def __getitem__(self, index):
-        print(len(self.image_paths))
+        clahe = cv2.createCLAHE(clipLimit=5, tileGridSize=(12, 12))
+        
+        
+        image = cv2.imread(self.image_paths[index])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, self.image_size)
+        image[:, :, 0] = clahe.apply(image[:, :, 0])
+        image[:, :, 1] = clahe.apply(image[:, :, 1])
+        image[:, :, 2] = clahe.apply(image[:, :, 2])
+        
+        mask = cv2.imread(self.mask_paths[index])
+        mask = cv2.resize(mask, self.image_size, interpolation=cv2.INTER_NEAREST)
+        mask = mask[:, :, 0]
+        mask = mask / 255.0
+        # print(image.shape, mask.shape)
+        
+        image_patches = image_to_patches(image, self.patch_size)
+        mask_patches = image_to_patches(mask, self.patch_size)
+        
+        image_patches = torch.tensor(image_patches, dtype=torch.float32).permute(0, 3, 1, 2)
+        mask_patches = torch.tensor(mask_patches, dtype=torch.float32).permute(0, 3, 1, 2)
+        return image_patches, mask_patches
+    
+    
+def image_to_patches(image, p_size):
+    """
+    Splits an image into patches of size p_size x p_size, padding the image if necessary.
+    
+    Parameters:
+    image (numpy array): Input image of shape (H, W, C) or (H, W).
+    p_size (int): Size of the patches to extract (p_size x p_size).
+    
+    Returns:
+    tuple: (Padded image, Patches in shape (num_patches_y, num_patches_x, p_size, p_size, C) 
+            if 3D input, else (num_patches_y, num_patches_x, p_size, p_size))
+    """
+    h, w = image.shape[:2]
+    
+    # Calculate the padding needed to make the dimensions divisible by p_size
+    pad_h = (p_size - h % p_size) % p_size
+    pad_w = (p_size - w % p_size) % p_size
+    
+    # Pad the image with zeros (or other values if necessary)
+    if len(image.shape) == 3:  # RGB or multi-channel image
+        image_padded = np.pad(image, ((0, pad_h), (0, pad_w), (0, 0)), mode='constant')
+    else:  # Grayscale image
+        image_padded = np.pad(image, ((0, pad_h), (0, pad_w)), mode='constant')
+    
+    # Get new dimensions after padding
+    h_padded, w_padded = image_padded.shape[:2]
+    
+    # Split the image into patches
+    patches = image_padded.reshape(h_padded // p_size, p_size, w_padded // p_size, p_size, -1)
+    patches = patches.swapaxes(1, 2)  # Move patch dimensions together
+    
+    num_patches_y, num_patches_x = patches.shape[:2]
+    patches = patches.reshape(num_patches_y * num_patches_x, p_size, p_size, -1)
+    
+    return patches
 
+
+def patches_to_image(patches, image_shape, p_size):
+    """
+    Reconstructs the original (padded) image from patches.
+    
+    Parameters:
+    patches (numpy array): Patches of shape (num_patches, p_size, p_size, C) or (num_patches, p_size, p_size).
+    image_shape (tuple): Shape of the original padded image (H, W, C) or (H, W).
+    p_size (int): Size of the patches (p_size x p_size).
+    
+    Returns:
+    numpy array: Reconstructed image of shape image_shape.
+    """
+    h_padded, w_padded = image_shape[:2]
+    
+    # Calculate the number of patches along height and width
+    num_patches_y = h_padded // p_size
+    num_patches_x = w_padded // p_size
+    
+    # Reshape patches back into the grid shape (num_patches_y, num_patches_x, p_size, p_size, C)
+    patches = patches.reshape(num_patches_y, num_patches_x, p_size, p_size, -1)
+    
+    # Swap axes back to combine patches into the original padded image
+    patches = patches.swapaxes(1, 2)
+    
+    # Reshape to the original image size
+    reconstructed_image = patches.reshape(h_padded, w_padded, -1)
+    
+    return reconstructed_image
+    
+    
+def rebatch(patches, batch_size):
+    num_patches = patches.shape[0]
+    # Split into sub-batches of the desired size
+    sub_batches = [patches[i:i + batch_size] for i in range(0, num_patches, batch_size)]
+    return sub_batches
 
 
 
@@ -37,8 +138,20 @@ def main():
     image_paths = sorted(list(image_dir.glob('*')))
     mask_paths = sorted(list(mask_dir.glob('*')))
     
-    for i, m in zip(image_paths, mask_paths):
-        print(i, m)
+    ds = BinSegDataset(image_paths=image_paths, mask_paths=mask_paths, image_size=(224*10, 224*10), patch_size=224)
+    dl = DataLoader(ds, batch_size=None, shuffle=True)
+    x, y = next(iter(dl))
+    print(x.shape)
+    
+    # print(len(ds))
+    # for image, mask in ds:
+    #     print(image.shape, mask.shape)
+    #     plt.subplot(1,2,1)
+    #     plt.imshow(image[2])
+    #     plt.subplot(1,2,2)
+    #     plt.imshow(mask[2], cmap='gray')
+    #     plt.show()
+    #     break
 
 
 
--- models.py
+++ models.py
@@ -1,18 +1,97 @@
 import torch
 from torch import nn
+import torchinfo
+import torchsummary
 
 class UNet(nn.Module):
-    def __init__(self, num_classes, ):
+    def __init__(self, in_channels, n_features, num_classes):
         super().__init__()
+        self.conv1 = self.conv_bn_relu(in_channels, n_features, 3)           # 224
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = self.conv_bn_relu(n_features, n_features * 2, 3)        # 112
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        self.conv3 = self.conv_bn_relu(n_features * 2, n_features * 4, 3)    #  56
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+        self.conv4 = self.conv_bn_relu(n_features * 4, n_features * 8, 3)    #  28
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+        self.conv5 = self.conv_bn_relu(n_features * 8, n_features * 8, 3)    #  14
+        self.pool5 = nn.MaxPool2d(kernel_size=2)
+        self.conv6 = self.conv_bn_relu(n_features * 8, n_features * 8, 3)    #   7
+        
+        self.conv06 = self.conv_bn_relu(n_features * 8, n_features * 8, 3)    #   7
+        self.upsample6 = nn.Upsample(scale_factor=2)
+        self.conv05 = self.conv_bn_relu(n_features * 16, n_features * 8, 3)    #  14
+        self.upsample5 = nn.Upsample(scale_factor=2)
+        self.conv04 = self.conv_bn_relu(n_features * 16, n_features * 8, 3)    #  28
+        self.upsample4 = nn.Upsample(scale_factor=2)
+        self.conv03 = self.conv_bn_relu(n_features * 12, n_features * 2, 3)    #  56
+        self.upsample3 = nn.Upsample(scale_factor=2)
+        self.conv02 = self.conv_bn_relu(n_features * 4, n_features * 1, 3)        # 112
+        self.upsample2 = nn.Upsample(scale_factor=2)
+        self.conv01 = self.conv_bn_relu(n_features, n_features, 3)           # 224
+        self.upsample1 = nn.Upsample(scale_factor=2)
+        
+        self.classifier = nn.Sequential(
+            nn.Conv2d(n_features * 2, n_features, kernel_size=3, padding='same'),
+            nn.Conv2d(n_features, num_classes, kernel_size=1, padding='same'),
+        )
+        
     
-    
-    def foward(self, x):
-        pass
-    
+    def forward(self, x):
+        x1 = self.conv1(x)
+        p1 = self.pool1(x1)
+        x2 = self.conv2(p1)
+        p2 = self.pool2(x2)
+        x3 = self.conv3(p2)
+        p3 = self.pool3(x3)
+        x4 = self.conv4(p3)
+        p4 = self.pool4(x4)
+        x5 = self.conv5(p4)
+        p5 = self.pool5(x5)
+        x6 = self.conv6(p5)
+        
+        x06 = self.conv06(x6)           # 7
+        u6 = self.upsample6(x06)        # 14
+        u6 = torch.concat([u6, x5], dim=1)
+        
+        x05 = self.conv05(u6)
+        u5 = self.upsample5(x05)        # 28
+        u5 = torch.concat([u5, x4], dim=1)
+        
+        x04 = self.conv04(u5)
+        u4 = self.upsample4(x04)        # 56
+        u4 = torch.concat([u4, x3], dim=1)
+        
+        x03 = self.conv03(u4)
+        u3 = self.upsample3(x03)        # 112
+        u3 = torch.concat([u3, x2], dim=1)
+        
+        x02 = self.conv02(u3)
+        u2 = self.upsample2(x02)        # 224
+        u2 = torch.concat([u2, x1], dim=1)
+        
+        return self.classifier(u2)
     
     def conv_bn_relu(self, in_channels, out_channels, kernel_size, ):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding='same'),
-            nn.BatchNorm2d(1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
+
+if __name__ == "__main__":
+    model = UNet(3, 32, 2).to('cuda')
+    # torchinfo.summary(model.to('cuda'), input_size=(1, 3, 224, 224))
+    torchsummary.summary(model, input_size=(3, 224, 224), batch_size=32)
+    
+    from torchviz import make_dot
+
+    # Create a dummy input
+    dummy_input = torch.randn(1, 3, 224, 224).type(torch.float32).to('cuda')  # Batch size of 1, input size of 10
+
+    # Generate the graph
+    output = model(dummy_input)
+    dot = make_dot(output, params=dict(list(model.named_parameters())))
+
+    # Render the graph
+    dot.render("model_architecture", format="png")
